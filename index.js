6295843df2a952662b28cd68cb672229af2bb878
Vinayak Koli <vinayakkoli9130@gmail.com>
2026-01-15 18:53:28 +0530

    add some routes and comments

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+//const express=require('express')();invoked 
+//or
 const express=require('express')//import express module
+
 
 //Creates an Express application. The express() function is a top-level 
 // function exported by the express module.
@@ -19,4 +22,11 @@ app.get("/contact",(req,resp)=>{
 app.get("/service",(req,resp)=>{
 resp.send("<h1>Welcome To The Service Page</h1>")
 })
+
+//Express matches and executes the first matching route.
+//So if "" and "/" look similar, only the first one is executed.
+app.get("",(req,resp)=>{
+resp.send("<h1>Second Home Page</h1>")
+})
+
 app.listen(3200)//port
