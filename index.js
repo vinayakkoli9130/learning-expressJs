5d379cf352884de262fe1bcd3f7d375db3e4197a
Vinayak Koli <vinayakkoli9130@gmail.com>
2026-01-15 19:35:18 +0530

    Refactor Express app to use ES imports/exports with modular pages

--- index.js
+++ index.js
@@ -1,32 +1,56 @@
-//const express=require('express')();invoked 
-//or
-const express=require('express')//import express module
+// //const express=require('express')();invoked 
+// //or
+// const express=require('express')//import express module//vanila js syntax
 
 
-//Creates an Express application. The express() function is a top-level 
-// function exported by the express module.
-const app=express()
+// //Creates an Express application. The express() function is a top-level 
+// // function exported by the express module.
+// const app=express()
+
+// app.get("",(req,resp)=>{
+// resp.send("<h1>Basic NodeJs Example</h1>")//Send a response.
+// })
+
+// app.get("/about",(req,resp)=>{
+//     resp.send("<h1>Welcome To The About Page</h1>")
+// })
+
+// app.get("/contact",(req,resp)=>{
+//     resp.send("<h1>Welcome To The Contact Page</h1>")
+// })
+
+// app.get("/service",(req,resp)=>{
+// resp.send("<h1>Welcome To The Service Page</h1>")
+// })
+
+// //Express matches and executes the first matching route.
+// //So if "" and "/" look similar, only the first one is executed.
+// app.get("",(req,resp)=>{
+// resp.send("<h1>Second Home Page</h1>")
+// })
+
+
+
+//--------------------------------------------------------------
+//use ES to import and export package.json:"type":"module"
+
+import express from 'express' //ECMAScript  syntax
+import home from './pages/home.js';
+import about, { contact } from './pages/about.js';
+
+let app=express()
 
 app.get("",(req,resp)=>{
-resp.send("<h1>Basic NodeJs Example</h1>")//Send a response.
+resp.send(home())
 })
 
 app.get("/about",(req,resp)=>{
-    resp.send("<h1>Welcome To The About Page</h1>")
+    resp.send(about(
+    ))
 })
 
 app.get("/contact",(req,resp)=>{
-    resp.send("<h1>Welcome To The Contact Page</h1>")
-})
-
-app.get("/service",(req,resp)=>{
-resp.send("<h1>Welcome To The Service Page</h1>")
-})
-
-//Express matches and executes the first matching route.
-//So if "" and "/" look similar, only the first one is executed.
-app.get("",(req,resp)=>{
-resp.send("<h1>Second Home Page</h1>")
+    resp.send(contact())
 })
 
 app.listen(3200)//port
