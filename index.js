dfea095f6befe9504bcf642ae74b4f9fb1e8a683
Vinayak Koli <vinayakkoli9130@gmail.com>
2026-01-15 18:15:18 +0530

    feat: initialize Express app with basic routes

---
+++ index.js
@@ -0,0 +1,22 @@
+const express=require('express')//import express module
+
+//Creates an Express application. The express() function is a top-level 
+// function exported by the express module.
+const app=express()
+
+app.get("",(req,resp)=>{
+resp.send("<h1>Basic NodeJs Example</h1>")//Send a response.
+})
+
+app.get("/about",(req,resp)=>{
+    resp.send("<h1>Welcome To The About Page</h1>")
+})
+
+app.get("/contact",(req,resp)=>{
+    resp.send("<h1>Welcome To The Contact Page</h1>")
+})
+
+app.get("/service",(req,resp)=>{
+resp.send("<h1>Welcome To The Service Page</h1>")
+})
+app.listen(3200)//port
